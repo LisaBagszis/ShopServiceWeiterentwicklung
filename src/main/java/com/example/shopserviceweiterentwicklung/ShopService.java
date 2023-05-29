eb6672a8c2be4efe2fd576ccca3381e7fa28e630
LisaBagszis <lisa.bagszis@gmail.com>
2023-05-29 23:24:51 +0200

    mit neuen Systemintegration Tests

--- src/main/java/com/example/shopserviceweiterentwicklung/ShopService.java
+++ src/main/java/com/example/shopserviceweiterentwicklung/ShopService.java
@@ -1,6 +1,6 @@
 package com.example.shopserviceweiterentwicklung;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
